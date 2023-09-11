880f981c3806ff539d55b1a06f3713b29b3051ec
Al Momin Faruk <31950303+mominfaruk@users.noreply.github.com>
2023-09-11 11:36:04 +0600

    update vercel json

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+echo "Start"
+python3.9 -m pip install -r requirements.txt 
+python3.9 manage.py collectstatic --noinput --clear
+echo "End"
