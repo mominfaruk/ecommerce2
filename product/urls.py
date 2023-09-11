1605b24c889759abd0058ca430714c191cae96f5
Al Momin Faruk <31950303+mominfaruk@users.noreply.github.com>
2023-09-11 14:37:54 +0600

    update vercel json

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('add_to_cart/',add_to_cart, name='add_to_cart'),
     path('update_cart/',update_cart,name='update_cart'),
     path('remove_from_cart/',remove_from_cart,name='remove_from_cart'),
